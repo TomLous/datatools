75835cb9e363fc15688c65f2c5f1d2c9e65298eb
tomlous <tomlous@gmail.com>
2014-05-06 21:59:49 +0200

    Geo

---
+++ v1/models/Geo.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author      Tom Lous <tomlous@gmail.com>
+ * @copyright   2014 Tom Lous
+ * @package     package
+ * Datetime:     06/05/14 21:59
+ */
+
+namespace models;
+
+
+class Geo {
+
+} 
