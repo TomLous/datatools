78d0682cedd1199eb810f355b43d2c98e32e541f
tomlous <tomlous@gmail.com>
2014-05-06 22:02:49 +0200

    Geo

---
+++ v1/models/Geolocation.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author      Tom Lous <tomlous@gmail.com>
+ * @copyright   2014 Tom Lous
+ * @package     package
+ * Datetime:     24/04/14 21:43
+ */
+
+namespace models;
+
+
+class Geolocation {
+
+} 
